b999f1e39fa0f022c6a28891b8fc699511d9b6f8
TomFiume100 <tomdiamonds100@gmail.com>
2020-09-15 21:26:06 +0000

    I made a snowman

--- index.java
+++ index.java
@@ -11,9 +11,20 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
-  
- 
-  
+  fill(255)
+  stroke(0)
+  ellipse(center)
+  ellipse(200,200,100,100)
+  ellipse(200,150,75,75)
+  ellipse(200,100,50,50)
+  fill(255, 171, 26)
+  rect(200,105,10,10)
+  fill(0)
+  ellipse(190,92,10,10)
+  ellipse(210,92,10,10)
+  stroke(106,41,41)
+  line(162,150,127,132)
+  line(238,150,273,132)
   
  
 
