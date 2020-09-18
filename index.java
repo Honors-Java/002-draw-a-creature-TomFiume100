3ab4e923a9a9c21ae56cb21b4c7c5e2a4a5bde37
TomFiume100 <tomdiamonds100@gmail.com>
2020-09-18 13:57:53 +0000

    I made some comments

--- index.java
+++ index.java
@@ -11,17 +11,23 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
+  
+  //These lines are here so that the entire thing is not orange
   fill(255)
   stroke(0)
   ellipse(center)
+  //This makes the snowman body
   ellipse(200,200,100,100)
   ellipse(200,150,75,75)
   ellipse(200,100,50,50)
+  //This fill makes the carrot square
   fill(255, 171, 26)
   rect(200,105,10,10)
+  //This fill makes the eyes
   fill(0)
   ellipse(190,92,10,10)
   ellipse(210,92,10,10)
+  //This stroke makes the color brown for the sitck arms
   stroke(106,41,41)
   line(162,150,127,132)
   line(238,150,273,132)
